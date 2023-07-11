feda425efcea273d3ea12940f6ff92451a20f84e
Hau Hin Lai <hlai23@myseneca.ca>
2023-07-11 09:02:21 -0400

    Add files via upload

---
+++ Workshops/cstring.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+// Intro. to Object Oriented Programming
+// Workshop 6 Part 1
+// Name: Hau Hin Lai
+// Student ID: 118795228
+// Email: hlai23@myseneca.ca
+// Section: ZAA
+// Date of completion: 10 July 2023
+
+   I have done all the coding by myself and only copied the code that my professor
+   provided to complete my workshops and assignments.
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+
+#ifndef SDDS_CSTRING_H_
+#define SDDS_CSTRING_H_
+namespace sdds {
+   void strCpy(char* des, const char* src);
+   unsigned strLen(const char* s);
+   void strCat(char* des, const char* src);
+}
+#endif // !SDDS_CSTRING_H_
+
+
+
